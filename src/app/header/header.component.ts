8b2ef4333f74c63edc371d4e1f6a47aac4524f94
kevinruse <kevin@kevinruse.com>
2020-02-20 14:40:17 -0800

    create and use a service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderTitleService } from '../services/header-title.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   userName: string = 'Kevin';
+  title: string;
 
-  constructor() { }
+  constructor(private headerTitleService: HeaderTitleService) {
+  }
+  ngOnInit() {
+    this.title = this.headerTitleService.getTitle();
+  }
+
 
 }

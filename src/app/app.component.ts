87bd536300001818c299859736d1d47ab5693655
kevinruse <kevin@kevinruse.com>
2020-02-21 10:38:45 -0800

    use @Output to share data from child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
     encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+
+    loggedIn: boolean;
+
     // tslint:disable-next-line:max-line-length
     user = {id: 1, name: 'Kevin', gender: 'M', ageGroup: '51+', userCode: 'M51+', reqs: {}, reqsStatus: {fruitMet: false, vegMet: false, proteinMet: false, grainMet: false}, registered: false, email: 'kevin@kevinruse.com'};
 
+    login(evt) {
+        console.log(evt);
+        this.loggedIn = evt;
+    }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Output()
+  login = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginOutput(login: boolean) {
+    this.login.emit(login);
+    console.log(login);
   }
 
 }

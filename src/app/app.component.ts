12d4516c27c0b0caae0b9aab9bc579894918e9bf
kevinruse <kevin@kevinruse.com>
2020-02-23 13:16:28 -0800

    add ngStyle directive - new exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 export class AppComponent {
 
     loggedIn: boolean = false;
+    stylePreference: string;
 
     // tslint:disable-next-line:max-line-length
     user = {id: 1, name: 'Kevin', gender: 'M', ageGroup: '51+', userCode: 'M51+', reqs: {}, reqsStatus: {fruitMet: false, vegMet: false, proteinMet: false, grainMet: false}, registered: false , email: 'kevin@kevinruse.com'};
@@ -18,4 +19,17 @@ export class AppComponent {
         this.loggedIn = evt;
         this.user.registered = !this.user.registered;
     }
+
+    setStylePreference(value) {
+        this.stylePreference = value;
+    }
+
+    setImportance() {
+        if (this.stylePreference === 'hilite') {
+           return { 'background-color' : 'yellow' };
+        } else if (this.stylePreference === 'caps') {
+            return { 'text-transform' : 'uppercase' };
+        }
+    }
+
 }
